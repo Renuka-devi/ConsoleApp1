e53cacb47828a82b4606ea1ce0029cc96c9b287e
renuka <renukaravi96@gmail.com>
2024-03-27 17:27:58 +0530

    Accept 3 numbers as i/p and print the greatest among them

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,16 +7,40 @@ using System.Threading.Tasks;
 namespace ConsoleApp1
 {
 	class Program
-	{
-		static void Main(string[] args)
-		{
-			var a = 10;
-			var b = "TEST0";
-			var c = 10.10;
-			Console.WriteLine("{0} type: {1}", a, a.GetType());
-			Console.WriteLine("{0} type: {1}", b, b.GetType());
-			Console.WriteLine("{0} type: {1}", c, c.GetType());
-			Console.ReadKey();
-		}
-	}
+    {
+        static void Main(string[] args)
+        {
+            var a = 10;
+            var b = "TEST0";
+            var c = 10.10;
+            Console.WriteLine("{0} type: {1}", a, a.GetType());
+            Console.WriteLine("{0} type: {1}", b, b.GetType());
+            Console.WriteLine("{0} type: {1}", c, c.GetType());
+
+            // reading numbers from console
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            //checking which one is greater
+            var flag = 0;
+            if(num1 > num2)
+            {
+                flag = 1;
+            }
+            if(flag == 1)
+            {
+                if(num1 > num3)
+                {
+                    Console.WriteLine(num1);
+                }
+            } else if (num2 > num3)
+            {
+                Console.WriteLine(num2);
+            } else
+            {
+                Console.WriteLine(num3);
+            }
+        }
+    }
 }
